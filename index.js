729124e51b9ffb8ce40833e2ca3d9aa16fa44b7f
Arc9067 <80489007+Arc9067@users.noreply.github.com>
2022-06-26 15:40:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
+
+if(window.innerWidth<900){
+
+  var tokenm= document.getElementById("tokenm").src="./imgs/tokenm.png"
+  
+  }
